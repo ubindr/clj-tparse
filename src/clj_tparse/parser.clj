37701c3d0f85acc452312a744743adb6fb633a9d
Richard Nagelmaeker <richard@bluesky.co.nl>
2016-11-19 21:52:57 +0100

    Processing subject iri's

--- src/clj_tparse/parser.clj
+++ src/clj_tparse/parser.clj
@@ -5,25 +5,40 @@
 
 (declare accept!)
 
+;; Temporary development variables
+
 (def tst-input "resources/example1.ttl")
 
+;; Constants
+
+(def base-prefix "ns")
+
+;; State variables
+
 (def parsing-prefixes (atom {}))
-(def global-prefixes (atom {}))
+(def dataset (atom {:prefixes {}
+                    :triples {}}))
+(def prefix-counter (atom 0))
 
-(def match-iri (re-pattern "^<(http://[^\u0000-\u0020<>\"{}|^`\\]+/)([^\u0000-\u0020<>\"{}|^`\\/]+)>$"))
+;; -- Regular expressions for symbols --
 
-;; Regular expressions for symbols
+;; Symbols used in other functions.
+
+(def split-iri (re-pattern "^<(http://[^\u0000-\u0020<>\"{}\\|^`]+[/#]{1})([^\u0000-\u0020<>\"{}|^`\\/]+)>$"))
+(def match-ns (re-pattern "^(http://[^\u0000-\u0020<>\"{}\\|^`]+[/#]{1})$"))
+
+;; Symbols for accept! function
 
 (def prefix-label (partial accept! "(^@prefix$|^prefix$|^PREFIX$){1}"))
 (def base-label (partial accept! "(^@base$|^base$|^BASE$){1}"))
 
-(defn next-symbol!
-  []
-  (swap! symbols rest))
+(defn nth-rest-vector
+  [n]
+  (fn [c] (vec (nthrest c n))))
 
-(defn next-two-symbols!
-  []
-  (swap! symbols #(rest (rest %))))
+(defn next-symbol!
+  ([] (next-symbol! 1))
+  ([n] (swap! symbols (nth-rest-vector n))))
 
 (defn accept!
   [re-s]
@@ -36,6 +51,16 @@
         false))
     nil))                                                   ;; Signaling symbols is empty
 
+(defn generate-prefix
+  []
+  (let [p-cnt (swap! prefix-counter inc)
+        prefix (str base-prefix p-cnt)]
+    (keyword prefix)))
+
+(defn register-prefix
+  [prefix iri]
+  (swap! dataset assoc :prefixes {prefix iri}))
+
 (defn return-prefix
   "Returns the correct prefix for a namespace.
   In case a prefix is supplied, it retrieves the namespace, to make sure it isn't used already.
@@ -46,31 +71,37 @@
     (keyword nspace)
     (let [ns (if (= ":" nspace) "local" nspace)
           lp-map @parsing-prefixes
-          gp-map @global-prefixes
-          absolute-url (or (first (match-url ns)) (lp-map (keyword ns)) (gp-map (keyword ns)))
+          gp-map (@dataset :prefixes)
+          absolute-url (or (first (re-matches match-ns ns)) (lp-map (keyword ns)) (gp-map (keyword ns)))
           local-nses (map-invert lp-map)
           nses (map-invert gp-map)
           a (or (and (local-nses absolute-url) 2) 0)
           b (or (and (nses absolute-url) 1) 0)
           g-prefix (nses absolute-url)
           l-prefix (local-nses absolute-url)]
-      (println "Prefix deciscion number" (+ a b))
+      (println "\nfn: return-prefix\n- prefix location state count:" (+ a b) "\n")
       (condp = (+ a b)
-        0 (build-prefix)
+        0 (let [prefix (generate-prefix)]
+            (register-prefix prefix absolute-url)
+            prefix)
         1 g-prefix
         2 (let [prefix (or (if (gp-map l-prefix)
                              false
                              l-prefix)
-                           (build-prefix))]
+                           (generate-prefix))]
             (register-prefix prefix absolute-url) prefix)
         3 g-prefix
-        (println "Something different" (+ a b))))))
+        (println "\nfn: return-prefix\n- Unexpected prefix state - parsing-prefixes:" a "- global-prefixes:" b "\n")))))
 
 (defn iri!
   []
   (let [iri (first @symbols)]
-    (->> (re-matches match-iri iri)
-         (partial return-prefix #(nth % 1)))))
+    (->> (re-matches split-iri iri)
+         ((juxt #(return-prefix (% 1)) #(% 2)))
+         (apply #(format "%s/%s" %1 %2))
+         rest
+         (apply str)
+         keyword)))
 
 (defn blank-node!
   [])
@@ -80,8 +111,6 @@
 
 (defn prefixed-name!
   [])
-
-
 
 (defn next-statement
   []
@@ -132,7 +161,7 @@
         iri (second @symbols)]
     (do
       (swap! parsing-prefixes assoc (keyword (str/replace prefix #":$" "")) (str/replace iri #"^<|>$" ""))
-      (next-two-symbols!)
+      (next-symbol! 2)
       (when (= (first @symbols) ".")
         (next-symbol!)))))
 
